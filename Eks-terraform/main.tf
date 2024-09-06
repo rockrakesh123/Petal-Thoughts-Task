9546025ae3eb90b0eb153880def884e15c8219fa
akhand6886 <aksingh6886@GMAIL.COM>
2024-09-06 20:57:00 +0530

    updated main.tf

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -128,9 +128,26 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
 }
 
 # IAM Role for ECS Task
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
 resource "aws_iam_role" "ecs_task_role" {
   name = "ecsTaskRole"
-
+  
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
